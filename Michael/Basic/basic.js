03dea1a020455de3e2b9dcf83e85b6e916ff149b
Michael Zechner <michael-zechner@gmx.at>
2021-09-30 11:54:56 +0200

    basic exercise

---
+++ Michael/Basic/basic.js
@@ -0,0 +1,13 @@
+let sandwiches2 = JSON.parse(sandwiches);
+let fries2 = JSON.parse(fries);
+console.log(sandwiches2);
+document.getElementById("message").innerHTML =
+  "My favourite sandwich is " +
+  sandwiches2.sandwich +
+  " which has approximately " +
+  sandwiches2.calories +
+  ", along with it I enjoy eating " +
+  fries2.fries_size +
+  " which have about " +
+  fries2.calories +
+  " calories.";
